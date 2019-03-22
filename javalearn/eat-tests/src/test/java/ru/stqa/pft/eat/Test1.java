4ed3ee8dbb5af0c01e6847a7bd26e93ab476d1b7
Kirill <ermakov9801@gmail.com>
2019-03-22 16:44:54 +0300

    Полина

--- javalearn/eat-tests/src/test/java/ru/stqa/pft/eat/Test1.java
+++ javalearn/eat-tests/src/test/java/ru/stqa/pft/eat/Test1.java
@@ -1,10 +1,12 @@
 package ru.stqa.pft.eat;
 
 import com.mysql.jdbc.Driver;
+import jdk.nashorn.internal.objects.NativeJSON;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import javax.swing.tree.RowMapper;
 import java.sql.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -34,7 +36,7 @@ public class Test1 {
         String password = "krlatq9bakd7e7pk";
         Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
         Connection conn = DriverManager.getConnection(url1, username, password);
-        PreparedStatement stmt2 = conn.prepareStatement("SELECT id FROM akeneo_pim.pim_catalog_product where JSON_EXTRACT(raw_values, \'$.\"Product_name\".\"<all_channels>\".\"<all_locales>\"\') = \'Автотест_03.03.2019_02:06:42\'");
+        PreparedStatement stmt2 = conn.prepareStatement("SELECT id FROM akeneo_pim.pim_catalog_product where JSON_EXTRACT(raw_values, \'$.\"Product_name\".\"<all_channels>\".\"<all_locales>\"\') = 'Автотест_03.03.2019_02:06:42'");
         stmt2.execute();
         ResultSet rs = stmt2.executeQuery();
 
@@ -46,6 +48,9 @@ public class Test1 {
         while (rs.next()) {
             System.out.println(rs.getString("id"));
         }
+
+
+        
     /*    String url1 ="jdbc:mysql://192.168.168.6:3306/magento2";
         String dbClass = "com.mysql.jdbc.Driver";
         Class.forName(dbClass).newInstance();
@@ -53,6 +58,7 @@ public class Test1 {
         Statement stmt = con.createStatement();
         ResultSet rs = stmt.executeQuery("select * from b2bclient_organization where inn = 7702651292;");
         System. out.println (rs);
+
 
 
 
